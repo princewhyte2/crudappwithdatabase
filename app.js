9b1f86ff241da24bcf97774f81788539e113143a
princewhyte2 <princewhyte02@gmail.com>
2021-05-10 14:30:36 +0100

    fix: correctly setup mongo db database from atlas

--- app.js
+++ app.js
@@ -7,14 +7,28 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var dataRouter = require("./routes/dataRouter");
+//const MongoClient = require("mongodb").MongoClient;
 
 const mongoose = require("mongoose");
 const Data = require("./models/data");
 
 const url =
-  "mongodb+srv://Princewhyte2:pC4W1qBuMaHbUOhx@cluster0.8jxlg.mongodb.net/data?retryWrites=true&w=majority";
-const connect = mongoose.connect(url);
+  "mongodb+srv://Princewhyte2:5rBgygL0TSScDRsO@cluster0.8jxlg.mongodb.net/data?retryWrites=true&w=majority";
 
+// const client = new MongoClient(url, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+const connect = mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+// client.connect((err) => {
+//   const collection = client.db("data").collection("data");
+//   console.log("Connected correctly to the server");
+//   // perform actions on the collection object
+//   client.close();
+// });
 connect.then(
   (db) => {
     console.log("Connected correctly to the server");
@@ -32,7 +46,7 @@ app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
